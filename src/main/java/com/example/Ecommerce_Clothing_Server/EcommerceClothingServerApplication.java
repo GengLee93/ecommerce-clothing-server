f21fa1b5e93020b54653a73fbd0a25ec3dd5d85c
GengL <lizhenggeng66@gmail.com>
2025-10-22 00:08:59 +0800

    Initial commit

---
+++ src/main/java/com/example/Ecommerce_Clothing_Server/EcommerceClothingServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.Ecommerce_Clothing_Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceClothingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceClothingServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Ecommerce_Clothing_Server/EcommerceClothingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Ecommerce_Clothing_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceClothingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
