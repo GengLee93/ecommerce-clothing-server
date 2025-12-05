5fbf2eb84b0004c64565f10dd12e2f44d4bd3969
GengL <lizhenggeng66@gmail.com>
2025-12-05 14:26:14 +0800

    Revert "Merge pull request #1 from GengLee93/feature/auth"
    This reverts commit 1a706d97f7590b363fc890f6edfadd2cc5c176f5, reversing
    changes made to f21fa1b5e93020b54653a73fbd0a25ec3dd5d85c.

--- src/main/java/com/example/Ecommerce_Clothing_Server/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Ecommerce_Clothing_Server.repository;
-
-import com.example.Ecommerce_Clothing_Server.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
